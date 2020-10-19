cfed72a709ccf25f2e5415d042e017c60be5ab34
s0310129 <70875110+s0310129@users.noreply.github.com>
2020-10-18 19:10:30 -0500

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Christopher Bowlds on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
